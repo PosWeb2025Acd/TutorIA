152c400f2d686ec64d3caf44fd5de1731e1920b5
Melques Paiva <melque1703@gmail.com>
2025-07-11 18:27:53 -0300

    V1 ACD: Ajustando função de pergunta para modelo para receber resultados da consulta ao banco

--- ACD/v1/main.py
+++ ACD/v1/main.py
@@ -2,7 +2,6 @@
 # Embedding model: https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2 (Modelo especializado em busca por similaridade de texto)
 # -- Modelo cria um vetores com 384 dimensões
 
-
 from db import get_db
 from model import question_to_model
 
@@ -12,10 +11,18 @@ if __name__ == "__main__":
     if db is None:
         print("Erro ao criar ou carregar o banco de dados.")
     else:
+        print ("Seja bem vindo!. Esse é o nosso TutorIA, agente especializado em responder perguntas sobre conceitos de POO.\n")
         while True:
-            print ("Seja bem vindo!. Esse é o nosso TutorIA, agente especializado em responder perguntas sobre conceitos de POO.\n")
+            print ("Digite a sua pergunta: ")
             question = input("👤 ")
             print ("Pensando...\n")
-            answer, sources = question_to_model(db, question)
+
+            # k = top 5 results
+            result_from_db = db.similarity_search_with_score(query=question, k=5)
+            if not result_from_db:
+                print("Não encontrei informações relevantes para responder a sua pergunta.\n")
+                continue
+
+            answer, sources = question_to_model(result_from_db, question)
             formated_response = f"Resposta: {answer}\nFontes:\n{"\n".join(sources)}"
             print(f"🤖 {formated_response}")
--- ACD/v1/model.py
+++ ACD/v1/model.py
@@ -1,10 +1,9 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
-from langchain_core.vectorstores import VectorStore
 
 MODEL = "deepseek-r1:8b"
 
-def question_to_model(db: VectorStore, question) -> str:
+def question_to_model(results_from_db, question):
     prompt = """
     You are an assistant for question-answering tasks related to computer science. Use the following pieces of retrieved context to answer the question.
     If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
@@ -13,13 +12,11 @@ def question_to_model(db: VectorStore, question) -> str:
     Think this step by step and provide a concise answer.
     """
 
-    # k = top 5 results 
-    results = db.similarity_search_with_score(query=question, k=5)
-    context = "\n\n".join([doc.page_content for doc, _ in results])
+    context = "\n\n".join([doc.page_content for doc, _ in results_from_db])
     prompt_template = ChatPromptTemplate.from_template(prompt)
 
     llm = OllamaLLM(model=MODEL, verbose=False, temperature=0.1, stop=["<think></think>"])
     llm_chain = prompt_template | llm
 
-    sources = [doc.metadata["page_id"] for doc, _ in results]
+    sources = [doc.metadata["page_id"] for doc, _ in results_from_db]
     return llm_chain.invoke({"context": context, "question": question}), sources
