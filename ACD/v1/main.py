82fecce6d308eb5b981a27e913bdbd6e9d3b0187
Melques Paiva <melque1703@gmail.com>
2025-07-13 15:27:33 -0300

    V1 ACD: Formatando resposta do modelo usando pydantic

--- ACD/v1/main.py
+++ ACD/v1/main.py
@@ -10,19 +10,21 @@ if __name__ == "__main__":
     db = get_db()
     if db is None:
         print("Erro ao criar ou carregar o banco de dados.")
-    else:
-        print ("Seja bem vindo!. Esse é o nosso TutorIA, agente especializado em responder perguntas sobre conceitos de POO.\n")
-        while True:
-            print ("Digite a sua pergunta: ")
-            question = input("👤 ")
-            print ("Pensando...\n")
+        exit(1)
 
-            # k = top 5 results
-            result_from_db = db.similarity_search_with_score(query=question, k=5)
-            if not result_from_db:
-                print("Não encontrei informações relevantes para responder a sua pergunta.\n")
-                continue
+    print ("Seja bem vindo!. Esse é o nosso TutorIA, agente especializado em responder perguntas sobre conceitos de POO.")
+    while True:
+        print ("Digite a sua pergunta: ")
+        question = input("👤 ")
+        print ("Pensando...")
 
-            answer, sources = question_to_model(result_from_db, question)
-            formated_response = f"Resposta: {answer}\nFontes:\n{"\n".join(sources)}"
-            print(f"🤖 {formated_response}")
+        # k = top 5 results
+        result_from_db = db.similarity_search_with_score(query=question, k=3)
+        if not result_from_db:
+            print("Não encontrei informações relevantes para responder a sua pergunta.")
+            continue
+
+        response, sources = question_to_model(result_from_db, question)
+
+        formated_response = f"Resposta: {response[0].answer}\nFontes:\n{"\n".join(sources)}"
+        # print(f"🤖 {formated_response}")
