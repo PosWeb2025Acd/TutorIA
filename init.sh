173472ff1c61f99ea4de59d3da665f23094bd042
Melques Paiva <melque1703@gmail.com>
2025-09-14 10:47:33 -0300

    Shell script para inicialização do projeto

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# SHOULD BE EXECUTED ONLY AT THE FIRST TIME RUNNING THE PROJECT
+
+docker compose up -d
+
+OLLAMA_SERVICE="ollama"
+RAG_SERVICE="api"
+
+OLLAMA_CONTAINER_ID=$(docker compose ps -q "$OLLAMA_SERVICE")
+RAG_CONTAINER_ID=$(docker compose ps -q "$RAG_SERVICE")
+
+if docker ps -q --no-trunc | grep -q "$OLLAMA_CONTAINER_ID"; then
+    printf "Ollama is running. Pulling LLMs: llama3.1"
+    docker compose exec -it $OLLAMA_SERVICE ollama pull llama3.1:8b
+else
+    printf "Ollama container is not running. Check if is a service running at the port 11434."
+fi
+
+if docker ps -q --no-trunc | grep -q "$RAG_CONTAINER_ID"; then
+    printf "\nRAG app is running"
+    docker compose exec -it $RAG_SERVICE ./app/scripts/init_checkpointer.sh
+else
+    printf "\nRAG app is not running"
+fi
--- scripts/init_checkpointer.sh
+++ scripts/init_checkpointer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 printf "Checkpoint setup initiated\n"
-/home/melques/Projects/TCC/TutorIA/ENV/bin/python3 checkpoint_setup.py
+python3 /app/scripts/checkpoint_setup.py
 printf "Checkpoint setup finished\n"
