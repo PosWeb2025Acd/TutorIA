0db6f5f9eb7b8fb151ee91bc29b63fe84b7e7689
Melques Paiva <melque1703@gmail.com>
2025-09-20 14:03:02 -0300

    Fechando cursor depois de consulta em DB

--- judge/user_and_answer/repository.py
+++ judge/user_and_answer/repository.py
@@ -6,14 +6,17 @@ def get_question_and_answers_without_evaluation(db_connection):
     cursor = db_connection.cursor()
 
     query = """
-    select qaa.id, qaa.question, qaa.answer  from question_and_answer qaa where qaa.id not in (select e.question_answer_id  from evaluation e );
+    select qaa.id, qaa.question, qaa.answer from question_and_answer qaa where qaa.id not in (select e.question_answer_id  from evaluation e );
     """
 
     cursor.execute(query)
     question_and_answer_list = cursor.fetchall()
 
     if not question_and_answer_list:
+        cursor.close()
         return None
+    
+    cursor.close()
     
     return question_and_answer_list
 
