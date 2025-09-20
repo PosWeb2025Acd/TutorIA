75dc5a2ef74780fb10f3e5f54bc25e5af7f924ee
Melques Paiva <melque1703@gmail.com>
2025-09-20 14:00:39 -0300

    Script único para criação inicial de tabelas do postgres

--- db-scripts/create_evaluation_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE evaluation(
-    id SERIAL PRIMARY KEY,
-    question_answer_id INTEGER NOT NULL,
-    llm_evaluation_reasoning TEXT NOT NULL,
-    llm_evaluation_score DECIMAL NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-ALTER TABLE evaluation ADD CONSTRAINT fk_evaluation__question_answer FOREIGN KEY (question_answer_id) REFERENCES question_and_answer(id);
---
+++ db-scripts/create_tables.sql
@@ -0,0 +1,32 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE usuarios (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    usuario VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP NULL
+);
+
+-- Índice para otimizar consultas por usuário
+CREATE INDEX idx_usuarios_usuario ON usuarios(usuario);
+
+CREATE TABLE question_and_answer(
+    id SERIAL PRIMARY KEY,
+    user_id uuid NOT NULL,
+    question text NOT NULL,
+    answer text NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_user_question_answer ON question_and_answer (user_id, question, answer);
+ALTER TABLE question_and_answer ADD CONSTRAINT fk_user_question_answer__user FOREIGN KEY (user_id) REFERENCES usuarios(id);
+
+CREATE TABLE evaluation(
+    id SERIAL PRIMARY KEY,
+    question_answer_id INTEGER NOT NULL,
+    llm_evaluation_reasoning TEXT NOT NULL,
+    llm_evaluation_score DECIMAL NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE evaluation ADD CONSTRAINT fk_evaluation__question_answer FOREIGN KEY (question_answer_id) REFERENCES question_and_answer(id);
