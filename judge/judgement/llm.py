d0c97687d0f4b9cae1d94da59858c0dff41abca6
Melques Paiva <melque1703@gmail.com>
2025-11-09 15:27:19 -0300

    Minor adjustments to judge prompt

--- judge/judgement/llm.py
+++ judge/judgement/llm.py
@@ -16,7 +16,7 @@ def evaluation_from_llm(question_to_evaluate, answer_to_evaluate) -> dict:
 
     prompt = """
     You are a grader assessing wheather an answer is userful to resolve a question. Give an grade score as an float on a scale of 0 to 10
-    , where 0 is when the answer is completely not useful to the question and 10 is when answer is complete useful to the questio. Provide
+    , where 0 is when the answer is completely not useful to the question and 10 is when answer is complete useful to the question. Provide
     the response as an JSON, with no preamble or explanation with two keys: score, and reasoning. The reasoning key should contain the explanation on why
     that score was provided.
 
